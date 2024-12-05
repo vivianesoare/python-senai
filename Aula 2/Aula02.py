842028abc7ab728274c3c1388a458ce6913042f3
vivianesoare <vivianesoaressantos962@gmail.com>
2024-12-05 09:56:15 -0300

    Aula02

---
+++ Aula 2/Aula02.py
@@ -0,0 +1,10 @@
+print("A educaçao transforma vidas!!")
+
+nome_usuario = "Viviane Soares " # nome
+idade_usuario = 18 # idade 
+altura_usuario = 1.56 # 
+maior_idade = verdadeiro 
+print("seja bem-vindo" , nome_usuario)
+print("sua idade é idade", usuario anos, "anos")
+print ("você tem",altura_usuario , "metros.") 
+print ("você é maior de idade?", maior _ de idade)
